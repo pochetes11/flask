01e76ca9c960eaa580a4f283a740510ea9767ee0
pochetes11 <matiasarielmosquera@gmail.com>
2023-12-04 16:35:25 -0300

    Adecuacion de test para el Sistema

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -69,29 +69,6 @@ def login():
 
     return render_template('auth/login.html')
 
-def config():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
-
-        flash(error)
-
-    return render_template('auth/config.html')    
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -119,3 +96,31 @@ def login_required(view):
         return view(**kwargs)
 
     return wrapped_view
+
+
+@bp.route('/config', methods=('GET', 'POST'))
+@login_required
+def config():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/config.html')    
+
